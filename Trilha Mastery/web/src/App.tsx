29ca74c0c3e9aaca9f74d2edc1da983b5d0629b0
jones-bass <jones-bass.tb@gmail.com>
2023-09-25 22:47:35 -0300

    Estrutura Front da App

--- Trilha Mastery/web/src/App.tsx
+++ Trilha Mastery/web/src/App.tsx
@@ -1,9 +1,83 @@
+import { Github, Wand2 } from 'lucide-react'
+
+import { Button } from "./components/ui/button"
+import { Separator } from './components/ui/separator'
+import { Textarea } from './components/ui/textarea'
+import { VideoInputForm } from './components/video-input-form'
+import { Label } from './components/ui/label'
 
 function App() {
 
   return (
-    <div className="min-h-screen flex flex-col bg-orange-400">
-    Ola BB
+    <div className="min-h-screen flex flex-col">
+    <div className="px-6 py-3 flex items-center justify-between border-b">
+      <h1 className="text-xl font-bold">upload.ai</h1>
+
+      <div className="flex items-center gap-3">
+        <span className="text-sm text-muted-foreground">
+          Desenvolvido com 💜 no NLW da Rocketseat
+        </span>
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <Button variant="outline">
+          <Github className="w-4 h-4 mr-2" />
+          GitHub
+        </Button>
+      </div>
+    </div>
+
+    <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA"
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{`{transcription}`}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6">
+          <VideoInputForm />
+
+          <Separator />
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              </div>
+
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <span className="block text-sm text-muted-foreground italic leading-relaxed">
+                Valores mais altor tendem a deixar o resultado mais criativo e com possíveis erros.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
+      </main>
+   
   </div>
   )
 }
