1486e69e1acf099715db8c932d40c1f2a9ec898a
jones-bass <jones-bass.tb@gmail.com>
2023-09-29 19:54:32 -0300

    Rota IA

--- Trilha Mastery/api/src/server.ts
+++ Trilha Mastery/api/src/server.ts
@@ -2,8 +2,13 @@ import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
 
 app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
